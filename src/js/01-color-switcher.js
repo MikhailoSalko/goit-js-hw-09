7b20834e96d39604ff480d40d222edc9ac1c814a
Mikhailo Salko <slipknotmisha@gmail.com>
2023-03-15 18:34:24 +0200

    solution to the first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.body;
+let changeColorId = null;
+stopBtn.setAttribute('disabled', true);
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', handleSetIntervalChangeBodyColor);
+stopBtn.addEventListener('click', handleStopChangeBodyColor);
+
+function handleSetIntervalChangeBodyColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  changeColorId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+}
+
+function handleStopChangeBodyColor(e) {
+  clearInterval(changeColorId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+}
