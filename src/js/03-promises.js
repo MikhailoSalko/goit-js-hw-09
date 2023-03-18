304241832a4bc18325fcf371cbd71e95f4472f06
Mikhailo Salko <slipknotmisha@gmail.com>
2023-03-18 13:52:08 +0200

    solution to the third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+
+const form = document.querySelector('.form');
+let formInput = {};
+
+form.addEventListener('submit', handleSubmitForm);
+form.addEventListener('input', debounce(handleInputValues, 200));
+
+function handleInputValues(e) {
+  formInput[e.target.name] = Number(e.target.value);
+
+  // console.log(formInput);
+  // return formInput;
+}
+
+function handleSubmitForm(e) {
+  e.preventDefault();
+  // console.log(formInput);
+
+  let { delay, step, amount } = formInput;
+  for (let i = 0; i < amount; i++) {
+    position = i + 1;
+    delay += step;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+
+  e.currentTarget.reset();
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
